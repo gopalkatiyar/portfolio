fbb0a68f8d9c7a37053372456a55954a6b7d65b9
gopal katiyar <gopalkatiyar2311@gmail.com>
2024-07-25 15:10:17 +0530

    fix issue

--- src/components/banner/RightBanner.jsx
+++ src/components/banner/RightBanner.jsx
@@ -5,15 +5,11 @@ const RightBanner = () => {
   return (
     <div className="w-1/2 flex relative ">
       <img
-        className="w-[120%] h-[100%] z-10"
+        className="w-[50] h-[70%] z-10 p-10"
         src={bannerImg}
         alt="bannerImg"
       />
-      <div
-        className="absolute bottom-0 w-[350px] h-[380px] ml-20 mb-[150px] lgl:w-[500px] lgl:h-[500px] 
-		bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex
-		 justify-center items-center"
-      ></div>
+     
     </div>
   );
 }
